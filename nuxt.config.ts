63219d1bf61c2d7c273fa20ae1a2fb5216a0f119
KnoX1568 <neofuenf+gitlab@googlemail.com>
2024-10-25 22:48:36 +0200

    added i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'node:path'
+import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   build: {
-    transpile: ['vuetify']
+    transpile: ['vuetify', 'vue-i18n']
   },
   modules: [
     '@nuxt/eslint',
@@ -22,5 +25,12 @@ export default defineNuxtConfig({
         transformAssetUrls,
       },
     },
+    plugins: [
+      VueI18nVitePlugin({
+        include: [
+          resolve(dirname(fileURLToPath(import.meta.url)), './locales/*.json')
+        ]
+      })
+    ]
   },
 })
